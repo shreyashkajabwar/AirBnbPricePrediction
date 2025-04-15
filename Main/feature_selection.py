a431adceda7fd1ee42e9dbba1c6caa1f86a348fa
shreyashkajabwar <124464834+shreyashkajabwar@users.noreply.github.com>
2025-04-16 01:29:37 +0530

    Update feature_selection.py

--- Main/feature_selection.py
+++ Main/feature_selection.py
@@ -1,14 +1,17 @@
 from sklearn import feature_selection
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy import stats
 import math
 
 
-X_train = pd.read_csv('../Data/data_cleaned_train_comments_X.csv')
-y_train = pd.read_csv('../Data/data_cleaned_train_y.csv')
+# All paths updated to absolute paths
+X_train = pd.read_csv('/Users/shreyashkajabwar/Desktop/AirBnbPricePrediction/Data/data_cleaned_train_comments_X.csv')
+y_train = pd.read_csv('/Users/shreyashkajabwar/Desktop/AirBnbPricePrediction/Data/data_cleaned_train_y.csv')
 
-X_val = pd.read_csv('../Data/data_cleaned_val_comments_X.csv')
-y_val = pd.read_csv('../Data/data_cleaned_val_y.csv')
+X_val = pd.read_csv('/Users/shreyashkajabwar/Desktop/AirBnbPricePrediction/Data/data_cleaned_val_comments_X.csv')
+y_val = pd.read_csv('/Users/shreyashkajabwar/Desktop/AirBnbPricePrediction/Data/data_cleaned_val_y.csv')
 
 F_vals, p_vals = feature_selection.f_regression(X_train, y_train)
 def clean_pvals(entry):
@@ -24,4 +27,4 @@ print(p_vals[p_vals<THRESH].shape)
 print(X_train.columns[p_vals<THRESH])
 print(list(X_train.columns[p_vals<THRESH]))
 print(len(list(X_train.columns[p_vals<THRESH])))
-np.save('../Data/selected_coefs_pvals.npy', X_train.columns[p_vals<THRESH])
+np.save('/Users/shreyashkajabwar/Desktop/AirBnbPricePrediction/Data/selected_coefs_pvals.npy', X_train.columns[p_vals<THRESH])
